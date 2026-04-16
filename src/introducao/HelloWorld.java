7818c08010278eb9a44e19367c402a7e057ca4f7
luana <luanamporto56@gmail.com>
2026-04-16 16:59:50 -0300

    inicializando meu projeto

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,12 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {  //método principal que executa codigo
+		// TODO Auto-generated method stub
+        //Nossa Lógica Aqui
+		
+		System.out.print("Hello World");  // identico ao escreva no portugol
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,24 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Variaveis
+		int idade = 25;
+		String nome = "Lilian";
+		float graus = 20.0f;   //float - ponto flutuante
+		boolean resposta;      // boolean - logico true/false
+		
+		//Constantes
+       final int QUANTIDADE = 50;
+      final String TURMA = "Turma 84";
+      final double AREA = 3.1415;
+      
+      System.out.println(nome);  //ln quebra de linha, espaço
+      System.out.println(QUANTIDADE);
+      System.out.println(graus);
+	}
+
+}
