1b60aff362994197255dcd609e9dce1fc9e71776
luana <luanamporto56@gmail.com>
2026-04-22 10:25:52 -0300

    Códigos sobre Estruturas Condicionais

---
+++ src/lacoCondicional/ExemploIfElse.java
@@ -0,0 +1,32 @@
+package lacoCondicional;
+
+import java.util.Scanner;
+
+public class ExemploIfElse {
+
+	public static void main(String[] args) {
+		
+		//Variaveis
+        float nota1, nota2, media;
+        Scanner leia = new Scanner(System.in);
+        
+        //Entrada de dados
+        System.out.println("Digite a primeira nota:");
+        nota1 = leia.nextFloat();
+        
+        System.out.println("Digite a segunda nota:");
+        nota2 = leia.nextFloat();
+        
+        //Processamento
+        media = (nota1 + nota2) /2;
+        if(media >=6) {
+        	System.out.println("Parabens, pessoa participante aprovada.");
+        }else {
+        	System.out.println("Infelizmente, a pessoa participante foi reprovada.");
+        }
+	}	
+        }
+        
+       
+       
+        	
