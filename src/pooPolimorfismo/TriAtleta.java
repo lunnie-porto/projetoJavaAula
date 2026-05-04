43964fdd15332e29770d548f3c403e7b6175f230
luana <luanamporto56@gmail.com>
2026-05-04 13:42:00 -0300

    POO - Final

---
+++ src/pooPolimorfismo/TriAtleta.java
@@ -0,0 +1,49 @@
+package pooPolimorfismo;
+
+public class TriAtleta extends Pessoa implements ICiclista {
+	
+	int competicoes;
+
+	// Métodos Construtores
+	public TriAtleta(String nome, int idade, int competicoes) {
+		super(nome, idade);	// Pessoa(nome, idade)
+		this.competicoes = competicoes;
+	}
+	
+	public TriAtleta(String nome, int idade) {
+		super(nome, idade);
+	}
+	
+	public int getCompeticoes() {
+		return competicoes;
+	}
+
+	public void setCompeticoes(int competicoes) {
+		this.competicoes = competicoes;
+	}
+	
+	public void competicoesConcluidas() {
+		System.out.println("Competições Concluídas: " + this.competicoes);
+	}
+	
+	// Polimorfismo de Sobrecarga (Sobrecarga de Método)
+	public void competicoesConcluidas(int numero) {
+		System.out.println("Competições Concluídas: " + numero);
+	}
+	
+	// Polimorfismo de Sobrescrita (Sobrescrita de Método)
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("Competições: " + this.competicoes);
+	}
+	
+	// Métodos Implementados das Interfaces
+	public void pedalar() {
+		System.out.println("Estou pedalando...");
+	}
+
+	@Override	// Anotação - Etiqueta de Dados/Info
+	public String aquecer() {
+		return "Estou me aquecendo...";
+	}
+}
